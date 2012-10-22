7f669c722cf57f9bf01b9fcfb2ab0bba1694ae2b
kane <kane@kanePC.(none)>
2012-10-22 00:43:39 -0700

    Circle and Rectangle intersection

--- gap/rectangle.py
+++ gap/rectangle.py
@@ -1,3 +1,5 @@
+import math
+
 class Rectangle:
    def __init__(self, x, y, w, h):
       self.x = x
@@ -11,6 +13,22 @@ class Rectangle:
             or self.y + self.h < other.y \
             or self.y > other.y + other.h)
 
+   def closest(self, val, minimum, maximum):
+      return max(minimum, min(val, maximum))
+
    def intersects_circle(self, circle):
-      # XXX
-      return False
+      closeX = self.closest(circle.position[0], self.x, self.x+self.w)
+      closeY = self.closest(circle.position[1], self.y, self.y+self.h)
+
+      dX = circle.position[0] - closeX
+      dY = circle.position[1] - closeY
+
+      distance = math.sqrt((dX * dX) + (dY * dY))
+
+      return distance < circle.radius
+
+
+   def __str__(self):
+      return "rectangle[%d, %d, %d, %d]" % (self.x, self.y, self.w, self.h)
+
+
